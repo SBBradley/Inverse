cc09804978ada46ad0a19b41ff46c70a7e34a414
SBBradley <SBBradley8@protonmail.com>
2022-03-22 10:25:02 +0900

    Formatted all files

--- Inverse.java
+++ Inverse.java
@@ -15,42 +15,41 @@ class Inverse {
     private static String fullNameInverse = "";
     private static char letter;
 
-    //The main method will gather user's name with a scanner.
+    // The main method will gather user's name with a scanner.
     public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-    try {
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println("Enter first name.");
-    firstName = scan.nextLine();
+        try {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("Enter first name.");
+            firstName = scan.nextLine();
 
-    System.out.println();
-    System.out.println("Enter middle name. (if no middle name, enter n/a)");
-    middleName = scan.nextLine();
+            System.out.println();
+            System.out.println("Enter middle name. (if no middle name, enter n/a)");
+            middleName = scan.nextLine();
 
-    System.out.println();
-    System.out.println("Enter last name.");
-    lastName = scan.nextLine();
+            System.out.println();
+            System.out.println("Enter last name.");
+            lastName = scan.nextLine();
 
-    scan.close();
+            scan.close();
 
-    System.out.println();
-    System.out.println("Inverse time..." + "\n" + "Your inversed name is:");
-    System.out.println();
-    System.out.println(combineFullName(firstName, middleName, lastName));
-    System.out.println();
+            System.out.println();
+            System.out.println("Inverse time..." + "\n" + "Your inversed name is:");
+            System.out.println();
+            System.out.println(combineFullName(firstName, middleName, lastName));
+            System.out.println();
+        } catch (Exception ex) {
+            System.out.println("\n" + "An error occurred" + "\n");
+            ex.printStackTrace();
+            System.exit(0);
+        }
     }
-    catch (Exception ex) {
-        System.out.println("\n" + "An error occurred" + "\n");
-        ex.printStackTrace();
-        System.exit(0);
-    }
-    }
 
-    //This method reverses user's middle name.
-   static String inverseFirstName(String firstName) {
+    // This method reverses user's middle name.
+    static String inverseFirstName(String firstName) {
 
         for (int i = 0; i < firstName.length(); i++) {
             letter = firstName.charAt(i);
@@ -60,20 +59,20 @@ class Inverse {
         return capitalizeString(firstNameInverse);
     }
 
-    //This method reverses user's middle name.
+    // This method reverses user's middle name.
     static String inverseMiddleName(String middleName) {
-        
+
         for (int i = 0; i < middleName.length(); i++) {
             letter = middleName.charAt(i);
             middleNameInverse = letter + middleNameInverse;
             middleNameInverse = middleNameInverse.toLowerCase();
-        } 
+        }
         return capitalizeString(middleNameInverse);
     }
 
-    //This method reverses user's last name.
+    // This method reverses user's last name.
     static String inverseLastName(String lastName) {
-        
+
         for (int i = 0; i < lastName.length(); i++) {
             letter = lastName.charAt(i);
             lastNameInverse = letter + lastNameInverse;
@@ -82,25 +81,25 @@ class Inverse {
         return capitalizeString(lastNameInverse);
     }
 
-    //This method combines each name into one full name depending on user's inputs.
+    // This method combines each name into one full name depending on user's inputs.
     static String combineFullName(String firstName, String middleName, String lastName) {
-        
+
         if (middleName.equalsIgnoreCase("n/a")) {
 
             fullNameInverse = inverseFirstName(firstName) + " " + inverseLastName(lastName);
-        }
-        else {
-            fullNameInverse = inverseFirstName(firstName) + " " + inverseMiddleName(middleName) + " " + inverseLastName(lastName);
+        } else {
+            fullNameInverse = inverseFirstName(firstName) + " " + inverseMiddleName(middleName) + " "
+                    + inverseLastName(lastName);
         }
         return fullNameInverse;
     }
-    
-    //Method to manually capitalize first letter in a string.
-   public static String capitalizeString(String input) {
-   String firstLetter = input.substring(0, 1);
-   String firstLetterCapitalized = firstLetter.toUpperCase();
-   String remainingString = input.substring(1);
-   String finalString = firstLetterCapitalized + remainingString;
-      return finalString;
-   }
+
+    // Method to manually capitalize first letter in a string.
+    public static String capitalizeString(String input) {
+        String firstLetter = input.substring(0, 1);
+        String firstLetterCapitalized = firstLetter.toUpperCase();
+        String remainingString = input.substring(1);
+        String finalString = firstLetterCapitalized + remainingString;
+        return finalString;
+    }
 }
--- InverseTest.java
+++ InverseTest.java
@@ -8,43 +8,43 @@ import org.junit.BeforeClass;
 */
 public class InverseTest {
 
-//Fixture initialization.
-@BeforeClass
-public void setUp() {
-}
+    // Fixture initialization.
+    @BeforeClass
+    public void setUp() {
+    }
 
-@Test 
-public void firstNameTest() {
-String firstNameActual = Inverse.inverseFirstName("Benjamin");
-String firstNameExpected = "Nimajneb";
-assertEquals(firstNameExpected, firstNameActual);
-}
+    @Test
+    public void firstNameTest() {
+        String firstNameActual = Inverse.inverseFirstName("Benjamin");
+        String firstNameExpected = "Nimajneb";
+        assertEquals(firstNameExpected, firstNameActual);
+    }
 
-@Test 
-public void middleNameTest() {
-String middleNameActual = Inverse.inverseMiddleName("Donny");
-String middleNameExpected = "Ynnod";
-assertEquals(middleNameExpected, middleNameActual);
-}
+    @Test
+    public void middleNameTest() {
+        String middleNameActual = Inverse.inverseMiddleName("Donny");
+        String middleNameExpected = "Ynnod";
+        assertEquals(middleNameExpected, middleNameActual);
+    }
 
-@Test
-public void lastNameTest() {
-String lastNameActual = Inverse.inverseLastName("Franklin");
-String lastNameExpected = "Nilknarf";
-assertEquals(lastNameExpected, lastNameActual);
-}
+    @Test
+    public void lastNameTest() {
+        String lastNameActual = Inverse.inverseLastName("Franklin");
+        String lastNameExpected = "Nilknarf";
+        assertEquals(lastNameExpected, lastNameActual);
+    }
 
-@Test
-public void combineFullNameTest1() {
-String combineFullNameActual = Inverse.combineFullName("benjamin", "donny", "franklin"); 
-String combineFullNameExpected = "Nimajneb Ynnod Nilknarf";
-assertEquals(combineFullNameExpected, combineFullNameActual);
-}
+    @Test
+    public void combineFullNameTest1() {
+        String combineFullNameActual = Inverse.combineFullName("benjamin", "donny", "franklin");
+        String combineFullNameExpected = "Nimajneb Ynnod Nilknarf";
+        assertEquals(combineFullNameExpected, combineFullNameActual);
+    }
 
-@Test
-public void combineFullNameTest2() {
-String combineFullNameActual = Inverse.combineFullName("benjamin", "n/a", "franklin"); 
-String combineFullNameExpected = "Nimajneb Nilknarf";
-assertEquals(combineFullNameExpected, combineFullNameActual);
-}
+    @Test
+    public void combineFullNameTest2() {
+        String combineFullNameActual = Inverse.combineFullName("benjamin", "n/a", "franklin");
+        String combineFullNameExpected = "Nimajneb Nilknarf";
+        assertEquals(combineFullNameExpected, combineFullNameActual);
+    }
 }
